aac66eaa595545f723349a260aa6153e883e0861
Xenia <k.nikolaevna98@gmail.com>
2026-01-23 02:28:45 +0300

    feat: добавляет роутинг и приватный роутинг

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,10 +1,40 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Layout from '../../layout/Layout';
+import PrivateRoute from '../../pages/private-route/private-route';
+
+import { AppRoute, AuthorizationStatus } from '../../const';
+
 import Main from '../../pages/main/main';
+import Favorites from '../../pages/favorites/favorites';
+import Login from '../../pages/login/login';
+import Offer from '../../pages/offer/offer';
+import PageNotFound from '../../pages/page-not-found/page-not-found';
 
 type AppScreenProps = {
   count: number;
 };
 
 function App({ count }: AppScreenProps) {
-  return <Main count={count} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Root} element={<Layout />}>
+          <Route index element={<Main count={count} />} />
+          <Route
+            path={AppRoute.Favorites}
+            element={
+              <PrivateRoute authorizationStatus={AuthorizationStatus.Auth}>
+                <Favorites />
+              </PrivateRoute>
+            }
+          />
+          <Route path={AppRoute.Login} element={<Login />} />
+          <Route path={AppRoute.Offer} element={<Offer />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 export default App;
--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,16 @@
 export const Setting = {
   OffersCount: 4,
 };
+
+export enum AppRoute {
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer:id',
+  Root = '/',
+}
+
+export const enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
